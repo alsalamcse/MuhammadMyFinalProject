10b794720b341dbfebbdc41d39f9ed848c1cd376
Owner <alsalamsce>
2019-02-20 09:26:20 +0200

    first step...

--- app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/SplashActivity.java
+++ app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.mousa.muhammad.muhammadmyfinalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,26 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
+    @Override
+    protected void onResume() {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        super.onResume();
+    }
+
+    public  class MyThread extends Thread
+    {
+        @Override
+        public void run() {
+
+            try {
+                sleep(3000);
+                Intent i=new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
