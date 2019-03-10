ce293820bab0c0d9f8e34bb43a401d19df7e4d19
alsalamcse <samih1079@danon.tzafonet.org.il>
2019-03-10 02:22:07 +0200

    Sign in activity finished java- function that asked if you are sure to close the app+ if you want Log in and click Log out in wrong with a pop- message

--- app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/ScheduleActivity.java
+++ app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/ScheduleActivity.java
@@ -1,30 +1,53 @@
 package com.mousa.muhammad.muhammadmyfinalproject;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import static com.mousa.muhammad.muhammadmyfinalproject.R.id.btnMyProfile;
+import static com.mousa.muhammad.muhammadmyfinalproject.R.id.col1row2;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    private TextView tvSchedule, tvMonth;
+    private TextView tvSchedule, tvMonth ,col1row1 , col2row1,col3row1;
     private Button btnMyProfile, btnAddWorker;
+    private Spinner spnMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
+        col1row1 = (TextView) findViewById(R.id.col1row1);
+        col2row1 = (TextView) findViewById(R.id.col2row1);
+        col3row1 = (TextView) findViewById(R.id.col3row1);
         tvSchedule = (TextView) findViewById(R.id.tvSchedule);
         tvMonth = (TextView) findViewById(R.id.tvMonth);
 
         btnAddWorker = (Button) findViewById(R.id.btnAddWorker);
         btnMyProfile = (Button) findViewById(R.id.btnMyProfile);
+
+//        spnMonth=(Spinner)findViewById(R.id.spnMonth);
+
+
+
+        col1row1.setText(getDate());
+        col2row1.setText(getDateTime());
+
 
         btnMyProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +57,38 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
 
+    }
+    //Following function getDateTime() returns datetime.
+    private String getDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    ////function to the button Back in phone asked you if you sure to Exit
+    public void onBackPressed(){
+        final AlertDialog.Builder builder= new AlertDialog.Builder(ScheduleActivity.this);
+        builder.setMessage("Really Exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                builder.setCancelable(false);
+            }
+        });
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/SignInActivity.java
+++ app/src/main/java/com/mousa/muhammad/muhammadmyfinalproject/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.mousa.muhammad.muhammadmyfinalproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +21,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class SignInActivity extends AppCompatActivity {
 
     private EditText edId, etPassWord;
-    private Button btnLogIN;
+    private Button btnLogIN, btnLogOut;
 
 
     FirebaseAuth auth;//to establish sign in sign up
@@ -34,11 +36,9 @@ public class SignInActivity extends AppCompatActivity {
         etPassWord=(EditText)findViewById(R.id .edPassWord) ;
 
         btnLogIN=(Button)findViewById(R.id .btnLogIn) ;
+        btnLogOut=(Button)findViewById(R.id .btnLogOut) ;
 
-
-
-
-//        btnSignUp.setOnClickListener(new View.OnClickListener() {
+//        btnLogOut.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
 //                // كود الانتقال إلى الشاشة الأخرى
@@ -46,29 +46,58 @@ public class SignInActivity extends AppCompatActivity {
 //                startActivity(i);
 //            }
 //        });
-        btnLogIN.setOnClickListener(new View.OnClickListener() {
+
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dataHandler();
+            public void onClick(View v) {
+
+                final AlertDialog.Builder builder= new AlertDialog.Builder(SignInActivity.this);
+                builder.setMessage("Are you sure?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //add the time to the table
+
+
+
+                        SignInActivity.super.onBackPressed();
+                    }
+                });
+                builder.setNegativeButton("No",null).setCancelable(false) ;
+                AlertDialog alert=builder.create();
+                alert.show();
+
             }
         });
 
 
 
+        btnLogIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder= new AlertDialog.Builder(SignInActivity.this);
+                builder.setMessage("Are you sure?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dataHandler();
+                    }
+                });
+                builder.setNegativeButton("No",null).setCancelable(false) ;
+                AlertDialog alert=builder.create();
+                alert.show();
 
-
-
+            }
+        });
     }
-
     private void dataHandler()
     {
         String id=edId.getText().toString();
         String passw=etPassWord.getText().toString();
         signIn(id,passw);
-
-
     }
-
+//
     private void signIn(String id,String passw) {
         FirebaseAuth auth=FirebaseAuth.getInstance();
         auth.signInWithEmailAndPassword(id,passw).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -90,5 +119,30 @@ public class SignInActivity extends AppCompatActivity {
         });
     }
 
+    ////function to the button Back in phone asked you if you sure to Exit
+    public void onBackPressed(){
+        final AlertDialog.Builder builder= new AlertDialog.Builder(SignInActivity.this);
+        builder.setMessage("Really Exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                builder.setCancelable(false);
+            }
+        });
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
+    }
+
+
+
+
 
 }
+
